403871126c3370090fd3c74f08e9dd592ff0b00b
Arix581 <113464283+Arix581@users.noreply.github.com>
2022-12-13 11:33:24 -0700

    Main.cs V1.1: added Players

--- Main.cs
+++ Main.cs
@@ -12,4 +12,29 @@ namespace myProgram
             //put code here
         }
     }
+    
+    class catanPlayer
+    {
+        public int ore;
+        public int brick;
+        public int sheep;
+        public int grain;
+        public int wood;
+        public string name;
+        
+        public catanPlayer(string _name)
+        {
+            ore = 0;
+            brick = 0;
+            sheep = 0;
+            grain = 0;
+            wood = 0;
+            name = _name;
+        }
+        
+        public void showStuff()
+        {
+            Console.WriteLine(name + " has " + ore + " ore, " + brick + " brick");
+        }
+    }
 }
